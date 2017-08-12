274eae280a79d035f0cdb1f3232f0ce8068ec62c
Ryan Kotzen <potz666@gmail.com>
2017-08-12 19:31:53 +0200

    playing around with callbacks.

--- index.js
+++ index.js
@@ -3,32 +3,66 @@
 const feathers = require('feathers');
 const NeDB = require('nedb');
 const service = require('feathers-nedb');
+const async = require('async');
+const dbPath = './data/users.db';
 
 const app = feathers().configure(services);
-
-const users = app.service('/users');
-
-Promise.all([
-    users.create({ email: 'jane.doe@gmail.com', password: '11111', role: 'admin' }),
-    users.create({ email: 'john.doe@gmail.com', password: '22222', role: 'user' }),
-    users.create({ email: 'judy.doe@gmail.com', password: '33333', role: 'user' })
-])
-    .then(results => {
-        console.log('created Jane Doe item\n', results[0]);
-        console.log('created John Doe item\n', results[1]);
-        console.log('created Judy Doe item\n', results[2]);
-
-        return users.find().then(results => console.log('find all items\n', results));
-    })
-    .catch(err => console.log('Error occurred:', err));
+const userService = app.service('/users');
 
 function services() {
     this.use('/users', service({ Model: userModel() }));
+    function userModel() {
+        return new NeDB({
+            filename: dbPath,
+            autoload: true
+        });
+    }
 }
 
-function userModel() {
-    return new NeDB({
-        filename: './data/users.db',
-        autoload: true
-    });
+async.series([addTestData], function(err) {
+    if (err) {
+        throw err;
+    }
+});
+
+function addTestData(callback) {
+    const usersToExist = [
+        { email: 'jane.doe@gmail.com', password: '11111', role: 'admin' },
+        { email: 'john.doe@gmail.com', password: '22222', role: 'user' },
+        { email: 'judy.doe@gmail.com', password: '33333', role: 'user' }
+    ];
+    async.each(usersToExist, ensureUserExists, callback);
 }
+
+function ensureUserExists(user, callback) {
+    userService.find({ email: user.email }, userFound);
+
+    function userFound(err, foundUsers) {
+        if (err) {
+            return callback(err);
+        }
+        if (foundUsers.length > 0) {
+            console.log('User already existed', foundUsers);
+            return callback();
+        }
+        userService.create(user, userCreated);
+    }
+
+    function userCreated(err, createdUser) {
+        if (err) {
+            return callback(err);
+        }
+        console.log('User created');
+        callback();
+    }
+}
+
+/*
+service methods
+find(params)
+get(id, params)
+create(data, params)
+update(id, data, params)
+patch(id, data, params)
+remove(id, params)
+*/
